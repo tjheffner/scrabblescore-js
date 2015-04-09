85948f81a2049667cd0b2211139f75ad5c19a329
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-09 08:56:27 -0700

    finish logic & all tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 var score = function(input) {
 
+  input = input.toUpperCase();
+
+  var letters = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1,
+                 'N': 1, 'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2,
+                 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4,
+                 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8,
+                 'Q': 10, 'Z': 10};
+
+  var output = 0;
+
+  for (var i = 0; i < input.length; i++) {
+    output += letters[input.charAt(i)] || 0;
+  }
+
+  /* setting output to also have || 0 means if something in the input is not found
+     in the object 'letters' it will just add 0, effectively ignoring it.
+  */
+  
+  return output;
 };
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,25 @@ describe('score', function () {
     it("will return a score of 1 for the letter 'a'", function() {
         expect(score("a")).to.equal(1);
     });
+
+    it("will return a score of 2 for the letters 'at'", function() {
+        expect(score("at")).to.equal(2);
+    });
+
+    it("will return a score of 5 for the letters 'bat'", function() {
+        expect(score("bat")).to.equal(5);
+    });
+
+    it("will return a score of 22 for the letters 'quiz'", function() {
+        expect(score("quiz")).to.equal(22);
+    });
+
+    it("will ignore numbers in the input and just return letter values", function() {
+        expect(score("at1")).to.equal(2);
+    });
+
+    it("will ignore punctuation in the input and just return letter values", function() {
+        expect(score("at,,,,")).to.equal(2);
+    });
+
 });
